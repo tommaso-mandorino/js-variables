ca0a1ed20cb1f9d005cf228338a07e0d53e49073
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-26 17:50:16 +0200

    feat(strings): finish exercise 2

--- js-variables/02-strings/script.js
+++ js-variables/02-strings/script.js
@@ -8,6 +8,12 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 1;
+const text = "text"
+const bool = true;
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
