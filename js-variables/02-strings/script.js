320f1c7f177a0fb70928ee9ed2c1e2365fe62db4
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-26 17:45:15 +0200

    feat(strings): finish exercise 1

--- js-variables/02-strings/script.js
+++ js-variables/02-strings/script.js
@@ -1,6 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = "Tommaso";
+console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
